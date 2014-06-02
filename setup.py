2ea664cc3413883dd77630c3dfe3798f0843dac0
Bernie Pope <florbitous@gmail.com>
2014-06-02 22:41:47 +1000

    Bumped version number to 1.2.0 because log/linear scale now supported

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='methpat',
-    version='1.1.0',
+    version='1.2.0',
     author='Bernie Pope',
     author_email='bjpope@unimelb.edu.au',
     packages=['methpat'],
